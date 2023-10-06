821faae181d44195b12ed577fe65da87e4118e58
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-06 23:39:09 +0200

    fix

--- src/app/api/get-schools/route.ts
+++
@@ -1,7 +0,0 @@
-import { getSchools } from "@/lib/scrapeFunctions";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const schools = await getSchools();
-  return NextResponse.json(schools);
-}
