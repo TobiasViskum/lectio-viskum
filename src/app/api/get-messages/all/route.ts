be06f2ef978f57d775a0f37dbdffbad80c0ab829
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-23 20:57:37 +0200

    Update

--- src/app/api/get-messages/all/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from "next/server";
-import { doMessagesRequest } from "../do-messages-request";
-
-export async function GET(request: NextRequest) {
-  return doMessagesRequest({
-    request: request,
-    type: "all",
-  });
-}
