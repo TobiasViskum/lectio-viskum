820c3be7db301aef1f5b7a3f073890b659ee4a15
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-27 20:56:01 +0200

    Update

--- src/library/scrapeFunctions/getSchedule/getEndTime.ts
+++
@@ -1,6 +0,0 @@
-export function getEndTime(lesson: Lesson) {
-  const splitEndTime = lesson.time.endTime.split(":");
-  const endTime = Number(splitEndTime[0]) + Number(splitEndTime[1]) / 60;
-
-  return endTime;
-}
