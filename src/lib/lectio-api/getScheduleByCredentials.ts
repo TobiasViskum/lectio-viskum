49467b935888b57071f6e6978dd48e34557bbf4f
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-28 23:39:09 +0200

    Update

--- src/lib/lectio-api/getScheduleByCredentials.ts
+++ src/lib/lectio-api/getScheduleByCredentials.ts
@@ -12,12 +12,12 @@ type FunctionProps = Prettify<
 
 export const getScheduleByCredentials = async (props: FunctionProps) => {
   const userId = props.userId;
-  const tag = `${userId}-schedule-${props.week + props.week}`;
+  const tag = `${userId}-schedule-${props.week + props.year}`;
   const foundCache = global.cache.get(tag);
 
-  if (foundCache && new Date().getTime() < foundCache.expires) {
-    return foundCache.data as MainType;
-  }
+  // if (foundCache && new Date().getTime() < foundCache.expires) {
+  //   return foundCache.data as MainType;
+  // }
 
   const result = await getSchedule({
     week: props.week,
