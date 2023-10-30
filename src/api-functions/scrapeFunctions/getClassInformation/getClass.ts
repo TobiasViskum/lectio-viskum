ae15ed595dfa4081a0c545bf227bc20a7b596c72
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-30 21:57:04 +0100

    Update

--- src/api-functions/scrapeFunctions/getClassInformation/getClass.ts
+++ src/api-functions/scrapeFunctions/getClassInformation/getClass.ts
@@ -1,10 +1,10 @@
 export function getClass(classStr: string, getFull: boolean = false) {
   classStr = classStr
     .trim()
-    .replace("Alle 1. G. elever", "1.G")
-    .replace("Alle 2. G. elever", "2.G")
-    .replace("Alle 3. G. elever", "3.G")
-    .replace("Alle 4. G. elever", "4.G")
+    .replace("Alle 1. G. elever", "Alle 1.G elever")
+    .replace("Alle 2. G. elever", "Alle 2.G elever")
+    .replace("Alle 3. G. elever", "Alle 3.G elever")
+    .replace("Alle 4. G. elever", "Alle 4.G elever")
     .replace("Alle Lærere", "Alle lærere")
     .replace("Udlån af lokaler", "");
 
