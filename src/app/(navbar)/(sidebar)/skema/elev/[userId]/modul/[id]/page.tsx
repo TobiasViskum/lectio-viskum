f764c4e8d597c1876a19a3a04b0fb8969d04a4b9
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-11-04 10:33:09 +0100

    Update

--- src/app/(navbar)/(sidebar)/skema/elev/[userId]/modul/[id]/page.tsx
+++ src/app/(navbar)/(sidebar)/skema/elev/[userId]/modul/[id]/page.tsx
@@ -1,13 +1,6 @@
-import { DocumentButton } from "@/components/global/DocumentButton";
 import { getLectioProps } from "@/lib/auth/getLectioProps";
 import { lectioAPI } from "@/lib/lectio-api";
-import { cn } from "@/lib/utils";
 import { capitalizeFirstLetter } from "@/util/capitalizeFirstLetter";
-import { load } from "cheerio";
-import Image from "next/image";
-import Link from "next/link";
-import React, { Fragment } from "react";
-import { TextPart } from "./_components/RenderHomework/Parts/TextPart";
 import { RenderHomework } from "./_components/RenderHomework";
 
 type Props = {
@@ -30,26 +23,24 @@ export default async function LessonPage({ params }: Props) {
     return <p>An error happened</p>;
   }
 
-  // console.log(lesson.other);
-
   const formattedStartTime = new Intl.DateTimeFormat("da-dk", {
-    weekday: "long",
-    day: "numeric",
-    month: "numeric",
+    dateStyle: "full",
+    timeStyle: "short",
   }).format(lesson.time.startDate);
   const formattedEndTime = new Intl.DateTimeFormat("da-dk", {
     dateStyle: "full",
+    timeStyle: "short",
   }).format(lesson.time.endDate);
 
   return (
     <div className="flex max-w-4xl flex-col gap-y-2">
       <p className="text-muted-foreground">
-        {capitalizeFirstLetter(formattedStartTime.replaceAll(".", "/"))}
+        {capitalizeFirstLetter(formattedStartTime)}
       </p>
       <p className="text-muted-foreground">
         {capitalizeFirstLetter(formattedEndTime)}
       </p>
-      <p>Fag: {lesson.subjects}</p>
+      <p className="text-xl">Fag: {lesson.subjects}</p>
       <div>
         <p className="pb-1 pt-4 text-sm text-muted-foreground">NOTE:</p>
         <p>{lesson.note}</p>
@@ -65,77 +56,6 @@ export default async function LessonPage({ params }: Props) {
               </div>
             );
           })}
-
-          {/* {lesson.homework.map((homework, index) => {
-            return (
-              <div key={index} className="py-2">
-                {homework.title.map((title) => {
-                  return (
-                    <p key={title.text} className="py-8 text-xl">
-                      {title.text}
-                    </p>
-                  );
-                })}
-                <div
-                  className={cn(
-                    "pb-1 text-xl",
-                    homework.titleHref ? "text-blue-400 underline" : "",
-                  )}
-                >
-                  {homework.titleHref ? (
-                    <>
-                      {homework.title.includes("/lectio/") ? (
-                        <DocumentButton
-                          strDocument={JSON.stringify({
-                            name: homework.title,
-                            href: homework.titleHref,
-                          })}
-                        />
-                      ) : (
-                        <Link href={homework.titleHref} target="_blank">
-                          {homework.title}
-                        </Link>
-                      )}
-                    </>
-                  ) : (
-                    <p>{homework.title}</p>
-                  )}
-                </div>
-                {homework.description.map((item, index) => {
-                  return (
-                    <div key={item.toString()}>
-                      {typeof item === "string" ? (
-                        <p className="text-sm">{item}</p>
-                      ) : Array.isArray(item) ? (
-                        item.map((str) => {
-                          return (
-                            <p key={str} className="text-sm">
-                              {str}
-                            </p>
-                          );
-                        })
-                      ) : "img" in item ? (
-                        <Image
-                          width={item.width}
-                          height={item.height}
-                          src={item.img}
-                          alt="img"
-                        />
-                      ) : "videoHref" in item ? (
-                        <iframe
-                          width="420"
-                          height="250"
-                          src={item.videoHref}
-                        ></iframe>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })} */}
         </div>
       </div>
       <div>
@@ -150,56 +70,6 @@ export default async function LessonPage({ params }: Props) {
               </div>
             );
           })}
-          {/* {lesson.other.map((homework) => {
-            return (
-              <div key={homework.title[0].text} className="py-2">
-                <div
-                  className={cn(
-                    "pb-1 text-xl",
-                    homework.titleHref ? "text-blue-400 underline" : "",
-                  )}
-                >
-                  {homework.titleHref ? (
-                    <DocumentButton
-                      strDocument={JSON.stringify({
-                        name: homework.title,
-                        href: homework.titleHref,
-                      })}
-                    />
-                  ) : (
-                    <p>{homework.title[0].text}</p>
-                  )}
-                </div>
-                {homework.description.map((item, index) => {
-                  return (
-                    <div key={item.toString()}>
-                      {typeof item === "string" ? (
-                        <p className="text-sm">{item}</p>
-                      ) : Array.isArray(item) ? (
-                        item.map((str) => {
-                          return (
-                            <p key={str} className="text-sm">
-                              {str}
-                            </p>
-                          );
-                        })
-                      ) : "img" in item ? (
-                        <Image src={item.img} alt="img" />
-                      ) : "videoHref" in item ? (
-                        <iframe
-                          width="420"
-                          height="250"
-                          src={item.videoHref}
-                        ></iframe>
-                      ) : (
-                        ""
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })} */}
         </div>
       </div>
     </div>
