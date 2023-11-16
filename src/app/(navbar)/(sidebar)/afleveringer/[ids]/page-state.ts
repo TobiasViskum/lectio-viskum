4b8584f8d63edc764aff91b5e2f6f0ddb3807840
TobiasViskum <tobias.t.viskum@gmail.com>
2023-11-16 19:14:09 +0100

    Update

--- src/app/(navbar)/(sidebar)/afleveringer/[ids]/page-state.ts
+++ src/app/(navbar)/(sidebar)/afleveringer/[ids]/page-state.ts
@@ -1,12 +1,8 @@
 import { getCachedAssignment } from "@/cache-functions/getCachedAssignment";
 import { lectioAPI } from "@/lib/lectio-api";
 
-let cachedAssignment: Promise<FullAssignment | null> = new Promise((resolve) =>
-  resolve(null),
-);
-let assignment: Promise<FullAssignment | null> = new Promise((resolve) =>
-  resolve(null),
-);
+let cachedAssignment: Promise<FullAssignment | null>;
+let assignment: Promise<FullAssignment | null>;
 
 export function getPageState() {
   return {
