11e6ea256aef18ac7b19d420e99a9d6c9beecf3e
TobiasViskum <tobias.t.viskum@gmail.com>
2023-11-24 12:57:53 +0100

    Update

--- src/app/(navbar)/beskeder/nyeste/page.tsx
+++ src/app/(navbar)/beskeder/nyeste/page.tsx
@@ -20,28 +20,28 @@ export default async function Messages() {
           {messages.map((message) => {
             return (
               <Fragment key={message.id}>
-                <div
-                  role="button"
-                  tabIndex={0}
-                  className="group flex items-start justify-start pb-2 pt-2 text-left first:pt-0 md:items-center"
-                >
+                <div className="group flex items-start justify-start pb-2 pt-2 first:pt-0 md:items-center">
                   <div className="mt-1 grid h-8 w-8 place-items-center">
                     {message.isUnread && (
                       <div className="h-3 w-3 rounded-full bg-blue-400" />
                     )}
                   </div>
-                  <div className="flex h-full w-full rounded-md group-[:hover:not(:has(#checkbox:hover))]:bg-accent group-[:hover:not(:has(#favorite:hover))]:bg-accent">
-                    <div className="md:item flex aspect-square h-fit w-fit flex-col place-items-center justify-center gap-2 p-1 md:flex-row">
-                      <Checkbox id="checkbox" className="hover:data" />
-                      <StarIcon
-                        id="favorite"
-                        className="h-6 w-6 p-0.5 text-orange-500"
-                      />
+                  <div
+                    role="button"
+                    tabIndex={0}
+                    className="flex w-full gap-x-2 rounded-md py-2 [&:hover:not(:has(#iconsHolder:hover))]:bg-accent"
+                  >
+                    <div
+                      className="flex flex-col justify-center gap-x-2 gap-y-1 md:flex-row md:items-center md:justify-start"
+                      id="iconsHolder"
+                    >
+                      <Checkbox className="m-0.5 h-4 w-4" />
+                      <StarIcon className="h-5 w-5 text-orange-500" />
                     </div>
-                    <div className="flex flex-col gap-x-10 py-1 pl-2 md:flex-row">
+                    <div className="flex flex-col items-center gap-y-1 text-left md:flex-row">
                       <p
                         className={cn(
-                          "mb-auto mt-auto w-80 items-center overflow-x-hidden text-ellipsis whitespace-nowrap text-justify text-sm md:w-44",
+                          "w-80 min-w-[320px] items-center overflow-x-hidden text-ellipsis whitespace-nowrap text-sm md:w-44 md:min-w-[176px]",
                           message.isUnread
                             ? "font-extrabold text-foreground"
                             : "font-bold text-muted-foreground",
@@ -51,7 +51,7 @@ export default async function Messages() {
                       </p>
                       <p
                         className={cn(
-                          "flex items-center text-sm",
+                          "w-full text-sm md:pl-4",
                           message.isUnread
                             ? "text-foreground"
                             : "text-muted-foreground",
