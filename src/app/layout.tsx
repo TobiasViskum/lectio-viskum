17167c4f6c8e219f8242a5871371d60d9affbce7
TobiasViskum <tobias.t.viskum@gmail.com>
2023-11-25 14:08:36 +0100

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default async function RootLayout({
     <html lang="da-dk">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body
         className={cn(
